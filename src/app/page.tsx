1e2e1812fa60c3c3a2f5643a28f2041968a11de0
BuildTools <unconfigured@null.spigotmc.org>
2024-03-06 00:00:53 +0100

    more stuff :O

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+import "./css/stylesheet.css"
+import "./css/rainbowtext.css"
+
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        
+    <main>
+        <div className="infocontainer">
+          <Avatar>
+            <AvatarImage src="https://cdn.discordapp.com/avatars/624861181311451137/2f361a44259edf650f4cc8af7c2787c0?size=1024" className="pfp"/>
+            <AvatarFallback>DR</AvatarFallback>
+          </Avatar>
+          <div className="name rainbow">Dean</div>
+        </div>
     </main>
   );
 }

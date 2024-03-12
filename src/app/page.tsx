2286e26e4b70913cb550d43089f723db6d3e8030
BuildTools <unconfigured@null.spigotmc.org>
2024-03-12 23:48:51 +0100

    Done for today :D

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button";
 import {Tooltip} from "@nextui-org/react";
+import { Separator } from "@/components/ui/separator"
+import {Tabs, Tab} from "@nextui-org/react";
+
+
 
 import "./css/stylesheet.css"
 import "./css/rainbowtext.css"
@@ -23,7 +27,21 @@ export default function Home() {
           <a href="https://github.com/Paasdag"><center><Button className="github-button bg-indigo-600 hover:bg-gray-800"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width={30} /></Button></center></a>
         </div>
         <div className="About-me">
-          
+          <div>
+            <h1 className="text-about-me">About me!</h1>
+          </div>
+          <Separator className="my-2"/>
+        <div>
+          <h2 className="about-text">
+            Hello!, i am Dean, A self taught programmer. Theres a few languages i'd like to learn like Java and get great at React, Html, Css to make websites like this one or even better!
+          </h2>
+          <Separator className="my-2"/>
+          <h1 className="text-about-me">Hobbies</h1>
+          <Separator className="my-2"/>
+          <h2 className="about-text">
+            I have a few hobbies for example Programming, Gaming, Cooking/Baking. I love cooking with my mother, If i had to say which my favorite thing is to make i'd say Brigadeiros they're from bazil and i love making these for special occasions like parties, gifts basically for anything!
+          </h2>
+        </div>
         </div>
     </main>
   );

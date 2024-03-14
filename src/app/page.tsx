d9bffc545a32e47af9f1d6e35e96199c9d5c24bf
BuildTools <unconfigured@null.spigotmc.org>
2024-03-14 20:48:54 +0100

    guh?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button";
 import {Tooltip} from "@nextui-org/react";
 import { Separator } from "@/components/ui/separator"
+import Image from 'next/image'
 
 
 
@@ -15,15 +16,17 @@ export default function Home() {
   return (
     <main>
         <div className="infocontainer">
-          <div><center><img src="https://cdn.discordapp.com/avatars/624861181311451137/2f361a44259edf650f4cc8af7c2787c0?size=1024" className="pfp"/></center></div>
+          <div><center>
+            <Image src="https://cdn.discordapp.com/avatars/624861181311451137/2f361a44259edf650f4cc8af7c2787c0?size=1024" className="pfp" alt="profile picture"/>
+            </center></div>
           <div className="name rainbow">Dean</div>
           <center><Badge className="badge">Coder & Managment</Badge></center>
           <Tooltip content="My discord is 'Dean.n.'" className="discord-button-tooltip">
             <Button className="discord-button hover:bg-zinc-700">
-              <img src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png" width={30}/>
+              <Image src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png" width={30} alt="idfk"/>
             </Button>
           </Tooltip>
-          <a href="https://github.com/Paasdag"><center><Button className="github-button bg-indigo-600 hover:bg-gray-800"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width={30} /></Button></center></a>
+          <a href="https://github.com/Paasdag"><center><Button className="github-button bg-indigo-600 hover:bg-gray-800"><Image src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width={30} alt=""/></Button></center></a>
         </div>
         <div className="About-me">
           <div>
